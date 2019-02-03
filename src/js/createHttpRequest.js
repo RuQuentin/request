50ac6a6528e31dce2b16fcc31d72e32420ae89b7
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-04 01:16:09 +0300

    refactor/eslint, fixed in createHttpRequest.js

--- src/js/createHttpRequest.js
+++ src/js/createHttpRequest.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 
 const httpRequestParams = {
-  baseUrl: 'http://localhost:8000',
-}
+  baseUrl: 'http://localhost:8000'
+};
 
 function createHttpRequest(params) {
   return new HttpRequest(params);
