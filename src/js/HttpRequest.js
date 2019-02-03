968bd187397b2434715aa80aa99b220db64ae73b
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-04 01:13:33 +0300

    refactor/eslint, fixed in HttpRequest.js

--- src/js/HttpRequest.js
+++ src/js/HttpRequest.js
@@ -19,20 +19,18 @@ class HttpRequest {
       responseType = 'json',
       onUploadProgress = null,
       onDownloadProgress = null
-      } = config || {};
+    } = config || {};
 
     const xhr = new XMLHttpRequest();
     const searchParams = new URLSearchParams();
     const finalUrl = new URL(url, this.baseUrl);
-    
-    Object.entries(params).map(([name, value]) => {
-      searchParams.append(name, value)
-    })
+
+    Object.entries(params).map(([name, value]) => searchParams.append(name, value));
 
     finalUrl.search = searchParams.toString();
     xhr.open(method, finalUrl);
 
-    const headersList = {...this.headers, ...headers};
+    const headersList = { ...this.headers, ...headers };
 
     for (const name in headersList) {
       xhr.setRequestHeader(name, headersList[name]);
@@ -44,28 +42,26 @@ class HttpRequest {
 
     const result = new Promise( (resolve, reject) => {
       xhr.onload = () => {
-        if (~~xhr.status/100 === 2) {
-          resolve(xhr)
+        if (Number.floor(xhr.status / 100) === 2) {
+          resolve(xhr);
         }
 
-        if (~~xhr.status/100 !== 2) {
-          reject(xhr)
+        if (Number.floor(xhr.status / 100) !== 2) {
+          reject(xhr);
         }
-      }
-      
+      };
+
       xhr.send(data);
-    })
+    });
 
-    return transformResponse.reduce( (chain, func) => {
-      return chain.then(data => func(data))
-    }, result)
+    return transformResponse.reduce((chain, func) => chain.then(data => func(data)), result);
   }
 
   get(url, config) {
-    return this.__request('GET', url, config)
+    return this.__request('GET', url, config);
   }
 
   post(url, config) {
-    return this.__request('POST', url, config)
+    return this.__request('POST', url, config);
   }
 }
